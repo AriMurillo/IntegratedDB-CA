1736157f107367961af67490d57ceccd94a212d5
RafaelNovais <54107877+RafaelNovais@users.noreply.github.com>
2023-12-21 18:23:27 +0000

    Update TaxCalculator.java
    Add USC and PRSI

--- src/integrateddb/ca/TaxCalculator.java
+++ src/integrateddb/ca/TaxCalculator.java
@@ -37,5 +37,34 @@ public class TaxCalculator {
 
         return tax;
     }
+
+    public static double calculateUSC(double income) {
+        double usc = 0;
+
+        if (income <= 12012) {
+            usc = 0.005 * income; // 0.5%
+        } else if (income <= 22110) {
+            usc = 0.02 * income; // 2% after 12012
+        } else if (income <= 69234) {
+            usc = 0.045 * income; // 4.5% after 22110
+        } else {
+            usc = 0.08 * income; // 8% after 69234
+        }
+        return usc;
+    }
+
+    public static double calculatePRSI(double income) {
+        double weeklyIncome = income / 52;
+        double prsi = 0;
+
+        if (weeklyIncome > 352) {
+            prsi = income * 0.04; // 4% of income
+            return prsi;
+        } else {
+            return 0 ;
+        }
+    }
+
+
 }
 
