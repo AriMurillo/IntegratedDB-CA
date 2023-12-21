225712506552a50e545324bbf4122dd74d3e6e28
AriMurillo <ari.murillo@outlook.es>
2023-12-21 11:40:24 +0000

    Update

--- src/integrateddb/ca/RegularUser.java
+++ src/integrateddb/ca/RegularUser.java
@@ -9,7 +9,7 @@ package integrateddb.ca;
  * @author Ari
  */
 public class RegularUser extends Users{
-    public RegularUser(String username, String password, String firstName, String lastName, String gender, String email, String marital_status, boolean if_married_both_work, boolean children, UserType userType) {
-        super(username, password, firstName, lastName, gender,email, marital_status, if_married_both_work, children,UserType.REGULAR_USER);
+    public RegularUser(String username, String password, String firstName, String lastName, String gender, String email, String marital_status, boolean if_married_both_work, boolean children, int id, int employeeID, UserType userType) {
+        super(username, password, firstName, lastName, gender,email, marital_status, if_married_both_work, children, id, employeeID, UserType.REGULAR_USER);
     }
 }
